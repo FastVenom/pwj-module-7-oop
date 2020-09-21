4a2ab84efbab7d3f35831875550bfc416967e35e
Bilal Ahmad <bilalbaheer1@gmail.com>
2020-09-20 22:20:45 -0700

    Intitialize color and element value. Also, set button color upon initialzating.

--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -2,7 +2,7 @@
 class Beat {
     constructor(audioSrc){
         this.audio= new Audio(audioSrc);
-        console.log(this.audio)
+      
     }
 }
     play = () => {
@@ -13,17 +13,25 @@ class Beat {
 
 
 class Button {
-    constructor(color, keyCode){
+    constructor(color, keyCode) {
+        this.color = color;
+        this.keyCode = keyCode;
+        this.element = document.getElementById(keyCode);
+        console.log(this.element);
+        this.setButtonColorInHTML
     }
 
     /**
      * Set the button color based on color specified
      */
     setButtonColorInHTML = () => {
+        this.element.style.borderColor = this.color
     }
 
     
     select = () => {
+        this.element.style.background = this.color
+        this.element.style.boxShadow = `0px 0x 17px 0px ${this.color}`
     }
 
     
