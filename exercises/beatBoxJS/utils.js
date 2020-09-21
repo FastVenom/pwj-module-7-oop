bd8d909c730c8c23741de1e22d40178fe53ef532
Bilal Ahmad <bilalbaheer1@gmail.com>
2020-09-20 22:42:47 -0700

    final changes and commit

--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -1,49 +1,40 @@
-
 class Beat {
     constructor(audioSrc){
-        this.audio= new Audio(audioSrc);
-      
+        this.audio = new Audio(audioSrc);
     }
-}
+
     play = () => {
         this.audio.currentTime = 0;
-        this.audio.play
+        this.audio.play();
     }
-
-
-
-class Button {
-    constructor(color, keyCode) {
-        this.color = color;
-        this.keyCode = keyCode;
-        this.element = document.getElementById(keyCode);
-        console.log(this.element);
-        this.setButtonColorInHTML
-    }
-
-setAnTransitionEndListener = () => {
-    this.element.addEventListener('transitioned', () => {
-        console.log("transition ended");
-        this.deselect();
-    })
 }
 
-    /**
-     * Set the button color based on color specified
-     */
+class Button {
+    constructor(color, keyCode){
+        this.element = document.getElementById(keyCode);
+        this.keyCode = keyCode;
+        this.color = color;
+        this.setButtonColorInHTML();
+        this.createTransitionEndListener();
+    }
+
     setButtonColorInHTML = () => {
-        this.element.style.borderColor = this.color
+        this.element.style.borderColor = this.color;
     }
 
-    
+    createTransitionEndListener = () => {
+        this.element.addEventListener("transitionend", ()=>{
+            this.deselect();
+        })
+    }
+
     select = () => {
-        this.element.style.background = this.color
-        this.element.style.boxShadow = `0px 0x 17px 0px ${this.color}`
+        this.element.style.backgroundColor = this.color;
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
     }
 
-    
     deselect = () => {
         this.element.style.backgroundColor = "transparent";
-        this.element.style.boxShadow = "none";
+        this.element.style.boxShadow = `none`;
     }
 }
